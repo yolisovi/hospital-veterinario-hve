347d8ab16dee80d259c74092c0b2672382ff7b92
AIMO <yolaovie@gmail.com>
2026-01-28 10:16:15 -0600

    Removiendo validaciones obligatorias en campos condicionales

--- forms.py
+++ forms.py
@@ -2,13 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, BooleanField, EmailField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired, Email
 
-
 class MultiCheckboxField(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
     option_widget = widgets.CheckboxInput()
-
-
-# ... (imports iguales) ...
 
 class RegistroCitaForm(FlaskForm):
     email = EmailField('Correo electrónico *', validators=[DataRequired(), Email()])
@@ -17,13 +13,24 @@ class RegistroCitaForm(FlaskForm):
     telefono = StringField('Teléfono', validators=[DataRequired()])
     mascota = StringField('Nombre Mascota', validators=[DataRequired()])
     edad = StringField('Edad', validators=[DataRequired()])
-    especie = SelectField('Especie', choices=[('', 'Seleccione...'), ('Perro', 'Perro'), ('Gato', 'Gato')], validators=[DataRequired()])
+    especie = SelectField('Especie',
+        choices=[('', 'Seleccione...'), ('Perro', 'Perro'), ('Gato', 'Gato')],
+        validators=[DataRequired()])
 
-    # Quitamos DataRequired de estos porque son condicionales
-    servicios_perro = MultiCheckboxField('Servicios Perros', choices=[...])
-    horario_perro = SelectField('Horario Perros', choices=[...]) # Sin DataRequired
+    # IMPORTANTE: Sin DataRequired() para que no bloqueen el envío
+    servicios_perro = MultiCheckboxField('Servicios Perros', choices=[
+        ('v_multiple', 'Vacuna múltiple: $450'), ('v_antirrabica', 'Vacuna antirrábica: $200'),
+        ('desp_int_menor', 'Despar. Interna <10kg: $240'), ('desp_int_mayor', 'Despar. Interna >10kg: $280')
+    ])
+    horario_perro = SelectField('Horario Perros', choices=[
+        ('', 'Seleccione horario...'), ('Lun24_12pm', 'Lunes 24 - 12 p.m.'), ('Mar25_9pm', 'Martes 25 - 9 p.m.')
+    ])
 
-    servicios_gato = MultiCheckboxField('Servicios Gatos', choices=[...])
-    horario_gato = SelectField('Horario Gatos', choices=[...]) # Sin DataRequired
+    servicios_gato = MultiCheckboxField('Servicios Gatos', choices=[
+        ('v_antirrabica_gato', 'Vacuna antirrábica: $200'), ('desp_int_gato', 'Desparasitación interna: $240')
+    ])
+    horario_gato = SelectField('Horario Gatos', choices=[
+        ('', 'Seleccione horario...'), ('Mar25_12pm', 'Martes 25 - 12 p.m.'), ('Mie26_10am', 'Miércoles 26 - 10 a.m.')
+    ])
 
     submit = SubmitField('Registrar Cita')

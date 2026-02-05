7bd14dba0da5c6ada9ef0435194a469b616caf63
yolisovi <yolaovie@gmail.com>
2026-02-05 16:17:09 -0600

    Sincronización automática de base de datos para UUID

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from hospvet import create_app
+from hospvet import create_app, db  # Importamos también 'db'
+import os
 
 app = create_app() #los parentesis ejecutan la aplicación
 
-
-import os
+# Bloque de auto-reparación de base de datos
+with app.app_context():
+    try:
+        db.create_all()
+        print("Base de datos sincronizada con éxito.")
+    except Exception as e:
+        print(f"Error al sincronizar BD: {e}")
 
 if __name__ == "__main__":
     # Esto es vital para que Render pueda "ver" tu app
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
-
-#arrancamos la aplicación
-# if __name__ == "__main__":
-#     #app.run(port=8000, debug=True, host="0.0.0.0") #cambiar debug = false
-#     app.run(port=8000, debug=True) #cambiar debug = false

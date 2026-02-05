eb16b66f9fd7e911bd205803d289f261754242a9
yolisovi <yolaovie@gmail.com>
2026-02-05 16:33:03 -0600

    Sincronización automática de base de datos para UUID

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-# from hospvet import create_app, db  # Importamos también 'db'
-# import os
-
-# app = create_app() #los parentesis ejecutan la aplicación
-
-# # Bloque de auto-reparación de base de datos
-# with app.app_context():
-#     try:
-#         db.create_all()
-#         print("Base de datos sincronizada con éxito.")
-#     except Exception as e:
-#         print(f"Error al sincronizar BD: {e}")
-
-# if __name__ == "__main__":
-#     # Esto es vital para que Render pueda "ver" tu app
-#     port = int(os.environ.get("PORT", 8080))
-#     app.run(host="0.0.0.0", port=port)
 from hospvet import create_app, db
 import os
 
@@ -24,7 +7,7 @@ with app.app_context():
     try:
         # ¡CUIDADO! Esto borrará los datos viejos de hace 8 días
         # Pero es la única forma de que se creen las columnas UUID y Nombre
-        db.drop_all()
+        #
         db.create_all()
         print("Base de datos RECONSTRUIDA con éxito (UUID activo).")
     except Exception as e:
